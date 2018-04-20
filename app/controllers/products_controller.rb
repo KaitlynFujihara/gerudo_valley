73a8f1c00b3321fc920f4dd47cc323364985d243
Guest User <Guest@Epicodus-4B.local>
2018-04-20 13:45:03 -0700

    edit form created for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,18 @@ class ProductsController < ApplicationController
      render :new
    end
  end
-
+ def edit
+   @product= Product.find(params[:id])
+   render :edit
+ end
+ def update
+   @product= Product.find(params[:id])
+   if @product.update(product_params)
+     redirect_to products_path
+   else
+     render :edit
+     end
+   end
  def destroy
    @product = Product.find(params[:id])
    @product.destroy

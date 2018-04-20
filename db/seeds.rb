b934242c9b048520fcfb0fc5fa017b7cf765b84a
Guest User <Guest@Epicodus-4B.local>
2018-04-20 13:30:43 -0700

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Product.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Zelda.item, cost: Faker::Number.between(1, 100), country: Faker::Address.country)
+  Product.create!(name: Faker::Zelda.item, cost: Faker::Number.between(1, 100), country: Faker::Zelda.location)
 end
 
 p "Created #{Product.count} products"

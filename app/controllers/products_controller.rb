79297992623da9b436c49b35f2877ea0c6e41a77
Guest User <Guest@Epicodus-4B.local>
2018-04-20 12:49:02 -0700

    get products to show up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,29 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+  def new
+   @product = Product.new
+ end
+ def create
+   @product = Product.new(fact_params)
+   if @product.save
+     redirect_to  products_path
+   else
+     render :new
+   end
+ end
+
+ def destroy
+   @product = Product.find(params[:id])
+   @product.destroy
+   redirect_to products_path
+ end
+ def show
+   @product = Product.find(params[:id])
+   render :show
+ end
+ private
+ def product_params
+   params.require(:product).permit(:name, :cost, :country)
+ end
 end
